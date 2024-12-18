63ee3755788be7c06fd4dcd0cc6f666ac3e7c2fc
hkreuter <heike.reuter@oxid-esales.com>
2024-12-18 10:15:13 +0100

    OXDEV-8637 Move codeception parameters into provider class

--- tests/Codeception/Config/params.php
+++ tests/Codeception/Config/params.php
@@ -7,43 +7,6 @@
 
 declare(strict_types=1);
 
-use OxidEsales\Codeception\Module\Database\DatabaseDefaultsFileGenerator;
-use OxidEsales\Facts\Config\ConfigFile;
-use OxidEsales\Facts\Facts;
-use Symfony\Component\Filesystem\Path;
+namespace OxidEsales\Graphql\Base\Tests\Codeception\Config;
 
-if ($shopRootPath = getenv('SHOP_ROOT_PATH')){
-    require_once(Path::join($shopRootPath, 'source', 'bootstrap.php'));
-}
-
-$facts = new Facts();
-$php = (getenv('PHPBIN')) ?: 'php';
-
-return [
-    'SHOP_URL' => $facts->getShopUrl(),
-    'SHOP_SOURCE_PATH' => $facts->getSourcePath(),
-    'VENDOR_PATH' => $facts->getVendorPath(),
-    'DB_NAME' => $facts->getDatabaseName(),
-    'DB_USERNAME' => $facts->getDatabaseUserName(),
-    'DB_PASSWORD' => $facts->getDatabasePassword(),
-    'DB_HOST' => $facts->getDatabaseHost(),
-    'DB_PORT' => $facts->getDatabasePort(),
-    'MODULE_DUMP_PATH' => getModuleTestDataDumpFilePath(),
-    'MYSQL_CONFIG_PATH' => getMysqlConfigPath(),
-    'PHP_BIN' => $php,
-];
-
-function getModuleTestDataDumpFilePath()
-{
-    return Path::join(__DIR__, '..', 'Support', 'Data', 'dump.sql');
-}
-
-function getMysqlConfigPath()
-{
-    $facts = new Facts();
-    $configFile = new ConfigFile($facts->getSourcePath() . '/config.inc.php');
-
-    $generator = new DatabaseDefaultsFileGenerator($configFile);
-
-    return $generator->generate();
-}
+return (new CodeceptionParametersProvider())->getParameters();
